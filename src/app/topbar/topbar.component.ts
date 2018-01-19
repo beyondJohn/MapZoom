c65aa7cc8607e26fe8248f24e36aa65d53885180
John Piesco <jp@Johns-iMac.local>
2018-01-19 18:06:32 -0500

    css change

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app-top',
-    template:`<div style='font-size:2em; padding: 20px 0;' class='title'>Test Dynamically Loaded HTML</div>`
+    template:`<div class='title toptile'>Test Dynamically Loaded HTML</div>`
 })
 export class TopComponent{
 
